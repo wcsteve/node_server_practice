09b4ec22312dbc7671162ad28850bdcbc4303bee
wcsteve <wcsteve9@gmail.com>
2018-04-21 16:57:52 -0700

    Add passport google oauth

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 //import in express library
+const passport = require('passport');
+//library that includes passport module
+const GoogleStrategy = require('passort-google-oauth20').Strategy;
+//import needed oauth strategy, only use one property: .Strategy
+
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
+//give passport library awareness of specific oauth strategy;
 
 const PORT = process.env.PORT || 5000;
 //set port to either Heroku provided, or 5000
