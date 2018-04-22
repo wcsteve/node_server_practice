8ab03eb8f30c5d47a655da984f85455e768c53d3
wcsteve <wcsteve9@gmail.com>
2018-04-21 17:34:09 -0700

    Secure oauth keys

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passort-google-oauth20').Strategy;
 //import needed oauth strategy, only use one property: .Strategy
 
-
 const app = express();
+
 passport.use(new GoogleStrategy());
 //give passport library awareness of specific oauth strategy;
 
